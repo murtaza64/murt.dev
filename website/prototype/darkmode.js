b8823661882b3c972e6e8120f9c19823d85d8825
Murtaza Javaid <murtaza.javaid@gmail.com>
2022-03-10 00:48:25 -0500

    added some basic content, created some cool styles

--- website/prototype/darkmode.js
+++ website/prototype/darkmode.js
@@ -1,11 +1,15 @@
 const toggleLight = e => {
-    document.documentElement.setAttribute("color-mode", "light");
-    console.log("hi!")
+    html.setAttribute("color-mode", "light");
 }
 const toggleDark = e => {
-    document.documentElement.setAttribute("color-mode", "dark");
+    html.setAttribute("color-mode", "dark");
 }
 const initColorMode = () => {
+    if (window.matchMedia("prefers-color-scheme: dark")) {
+        html.setAttribute("color-mode", "dark");
+    } else {
+        html.setAttribute("color-mode", "dark");
+    }
     document.querySelector("#toggle-light").addEventListener("click", toggleLight);
     document.querySelector("#toggle-dark").addEventListener("click", toggleDark)
 }
--- website/prototype/onload.js
+++ website/prototype/onload.js
@@ -1,4 +1,7 @@
+let html = document.documentElement;
+html.setAttribute("js", true);
 document.querySelectorAll(".js-disabled-hide").forEach(el => {
     el.classList.remove("js-disabled-hide");
 })
+console.log("javascript is on!")
 initColorMode();
